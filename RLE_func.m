140b988e86802ab541c4f47744f9e6254e701723
kautilyajoshi-ujn <55709233+kautilyajoshi-ujn@users.noreply.github.com>
2020-04-06 00:05:40 +0530

    Update RLE_func.m

--- RLE_func.m
+++ RLE_func.m
@@ -1,12 +1,12 @@
-function d = my_RLE(x)
-ind=1;
-d(ind,1)=x(1);
-d(ind,2)=1;
+function d = RLE_func(x)
+var=1;
+d(var,1)=x(1);
+d(var,2)=1;
 for i=2 :length(x)
     if x(i-1)==x(i)
-       d(ind,2)=d(ind,2)+1;
-    else ind=ind+1;
-         d(ind,1)=x(i);
-         d(ind,2)=1;
+       d(var,2)=d(var,2)+1;
+    else var=var+1;
+         d(var,1)=x(i);
+         d(var,2)=1;
     end
 end
