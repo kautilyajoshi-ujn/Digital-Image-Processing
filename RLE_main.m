b268acfb323024bbdbb45abde3a4b4bd06231ef9
kautilyajoshi-ujn <55709233+kautilyajoshi-ujn@users.noreply.github.com>
2020-04-06 00:07:28 +0530

    Update RLE_main.m

--- RLE_main.m
+++ RLE_main.m
@@ -6,13 +6,13 @@ clc; clear;close all;
 vec = randi([0 1],1,100);
 
 % Use Function 
-d = my_RLE(vec);
-len_out = length(d);
-len_ip = length(vec);
+d = RLE_func(vec);
+out = length(d);
+ip = length(vec);
 
 %Compare length 
-if (len_out / len_ip) <1
-    fprintf('Positive Compression with ratio = %.2d\n', len_out / len_ip)
+if (out / ip) <1
+    fprintf('Positive Compression with ratio = %.2d\n', out / ip)
 else
     fprintf('Negative compression.')
 end
